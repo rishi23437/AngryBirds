4955d2fd2c05e343481a7b219038fd4ecf4b731c
Rishi <162436203+rishi23437@users.noreply.github.com>
2024-12-01 11:46:47 +0530

    Update TestSetNextBird.java

--- test/TestSetNextBird.java
+++ test/TestSetNextBird.java
@@ -1,41 +1,13 @@
 package io.github.angry_birds;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import org.junit.jupiter.api.BeforeAll;
 
 public class TestSetNextBird {
-    // Tests the set_next_bird() method of the Slingshot class
-
-//    @BeforeAll
-//    static void setup() {
-//        // Initialize the headless backend
-//        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-//        new HeadlessApplication(new ApplicationListener() {
-//            @Override
-//            public void create() {}
-//            @Override
-//            public void resize(int width, int height) {}
-//            @Override
-//            public void render() {}
-//            @Override
-//            public void pause() {}
-//            @Override
-//            public void resume() {}
-//            @Override
-//            public void dispose() {}
-//        }, config);
-//    }
-
-
     @Test
     void testSetNextBird() {
         Slingshot test_slingshot = new Slingshot();
