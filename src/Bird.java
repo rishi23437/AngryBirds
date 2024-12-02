6bd9516512866a91d95c24f1e1f5d06ab5301c9f
5aha5 <162083187+5aha5@users.noreply.github.com>
2024-12-02 14:56:36 +0530

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -8,18 +8,23 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public abstract class Bird {
     public Vector2 initialBirdPosition;
+    private boolean stoppedmoving = false;
     private boolean hasBeenTouched = false;
-    // ADD attribute representing MASS - maybe int mass
-
+    int Bird_Health;
     Texture bird_image;
-    public abstract void create(World world);
+    Body birdBody;
 
+    public abstract void create(World world);
     public abstract CircleShape getCircle();
     public abstract Body getBirdBody();
     public abstract float get_radius();
 
     public Texture getBirdImage() {
         return bird_image;
+    }
+    public void setPosition(float x, float y) {
+        initialBirdPosition = new Vector2(x, y);
+        birdBody.setTransform(x / Level.PPM, y / Level.PPM, 0);
     }
 
     public boolean get_HasBeenTouched() {
@@ -28,5 +33,24 @@ public abstract class Bird {
 
     public void set_HasBeenTouched(boolean hasBeenTouched) {
         this.hasBeenTouched = hasBeenTouched;
+    }
+
+    public int getBird_Health() {
+        return Bird_Health;
+    }
+    public void setBird_Health(int Bird_Health) {
+        this.Bird_Health = Bird_Health;
+    }
+
+    public void dispose() {
+        birdBody.getWorld().destroyBody(birdBody);
+        bird_image.dispose();
+    }
+
+    public boolean getStoppedmoving() {
+        return stoppedmoving;
+    }
+    public void setStoppedmoving(boolean stoppedmoving) {
+        this.stoppedmoving = stoppedmoving;
     }
 }
